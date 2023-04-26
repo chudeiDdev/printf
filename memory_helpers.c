197cfff8d00a7b7e39a9288347e8353e293deb55
officialezekiel <officialezekiel1@gmail.com>
2023-04-25 17:57:38 -0700

    memory correction

--- memory_helpers.c
+++ memory_helpers.c
@@ -15,7 +15,7 @@ buffer_t *init_buffer(void);
  */
 unsigned int _memcpy(buffer_t *output, const char *src, unsigned int n)
 {
-	int index;
+	unsigned int index;
 
 	for (index = 0; index < n; index++)
 	{
